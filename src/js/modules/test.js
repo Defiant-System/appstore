9eb4cd71915bfb58f17286e6a355ba544d2e4e96
Hakan Bilgin <hbi99@hotmail.com>
2024-09-01 20:12:12 +0200

    Update test.js

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -1,7 +1,7 @@
 
 let Test = {
 	init() {
-		setTimeout(() => window.find(`.categories li[data-id="themes"]`).trigger("click"), 200);
+		// setTimeout(() => window.find(`.categories li[data-id="system"]`).trigger("click"), 200);
 		// setTimeout(() => window.find(`.button[data-arg="ant:2048"]`).trigger("click"), 1000);
 		// setTimeout(() => window.find(".reel-nav li:nth(1)").trigger("click"), 1000);
 	}
